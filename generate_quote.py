b217aac79e9f358ca0df9952d66379cbdb30134f
Dev Prakash Sharma <idevprakaash@gmail.com>
2024-09-30 04:11:22 +0530

    Update generate_quote.py

--- generate_quote.py
+++ generate_quote.py
@@ -1,7 +1,7 @@
 import requests
 
 def fetch_quote():
-    url = "https://api.api-ninjas.com/v1/quotes"  
+    url = "https://api.api-ninjas.com/v1/quotes?category=knowledge"  
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
