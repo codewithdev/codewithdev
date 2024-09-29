966e72e6b161e7c95bf017a6ca693d29af393d8a
Dev Prakash Sharma <idevprakaash@gmail.com>
2024-09-30 04:32:04 +0530

    Update generate_quote.py

--- generate_quote.py
+++ generate_quote.py
@@ -8,18 +8,20 @@ def fetch_quote():
         "X-Api-Key": api_key 
     }
     response = requests.get(url, headers=headers)
+    print(f"Response Status Code: {response.status_code}")
+    print(f"Response JSON: {response.json()}")
     if response.status_code == 200:
         data = response.json()
         if isinstance(data, list) and len(data) > 0:
-            quote_data=data[0]
+            quote_data = data[0]
             return f"{quote_data['quote']} - {quote_data['author']}"
         else:
             return "No quotes for today."
     else:
         return "Could not fetch a quote."
-        
 
 quote = fetch_quote()
+print(f"Fetched Quote: {quote}")
 
 # Update the README file with the new quote
 with open("README.md", "r") as file:
